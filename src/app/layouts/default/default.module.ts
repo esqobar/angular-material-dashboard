0956f21e94193a25ef00336b6162f5a215585217
esqobar <collecollins@gmail.com>
2019-11-15 18:17:19 +0300

    first commit

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule, MatDividerModule, MatListModule, MatCardModule, MatPaginatorModule, MatTableModule} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DashboardService } from 'src/app/modules/dashboard.service';
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatTableModule
+    
+  ],
+  providers: [
+    DashboardService
+  ]
+})
+export class DefaultModule { }
